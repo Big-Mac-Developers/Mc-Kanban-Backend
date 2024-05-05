921f3c6e7604ff5561ea01cf2301b7da5512ee4c
cooi0004 <cooi0004@student.monash.edu>
2024-05-05 19:34:02 +1000

    add route for upload

--- src/upload/router.py
+++ src/upload/router.py
@@ -1,7 +1,21 @@
-from fastapi import APIRouter, Request, File, UploadFile
+from fastapi import APIRouter, Request, File,Form, UploadFile
+from pypdf import PdfReader
+import io
+from src.ai.ai import extract_tasks_from_text
 
 upload_router = APIRouter()
-@upload_router.post("/upload")
-async def upload_file(request:Request, uploadFile:UploadFile):
-    file_content = await uploadFile.read()
-    return {"filename": uploadFile.filename, "content": file_content}
+
+@upload_router.post("/")
+async def upload_file(request: Request,
+                      file: UploadFile= File(None),
+
+                      ):
+    file_content = await file.read()
+    stream = io.BytesIO(file_content)
+
+    reader = PdfReader(stream)
+    number_of_pages = len(reader.pages)
+    text = ''.join([page.extract_text() for page in reader.pages])
+    response = extract_tasks_from_text(text)
+
+    return response
